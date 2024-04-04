a44ec551948d7b03b472b96a2c4093e27b41d6dc
Vinicius Fillos <vinifillos@gmail.com>
2024-04-03 23:42:34 -0300

    Feat: program implemented using wildcard types

--- src/application/App.java
+++ src/application/App.java
@@ -1,15 +1,23 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
 
-        List<Object> myObjs = new ArrayList<>();
-        List<Integer> myNumbers = new ArrayList<>();
+        List<Integer> myInts = Arrays.asList(5,2,10);
+        printList(myInts);
 
-        myObjs = myNumbers;
+        List<String> myStrings = Arrays.asList("Maria", "Pietra", "João");
+        printList(myStrings);
+    }
+
+    public static void printList(List<?> list) {
+        System.out.println();
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
     }
 }
